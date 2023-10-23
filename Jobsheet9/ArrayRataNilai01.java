a1223560ee1a34a94cbb6c2f42e0a84b68e97665
abdillah <abdillahnoersaid@gmail.com>
2023-10-23 12:59:09 +0700

    Jobsheet 9

---
+++ Jobsheet9/ArrayRataNilai01.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ArrayRataNilai01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nilaiMhs = new int[10];
+        double total = 0;
+        double rata2;
+        int mhsLulus = 0;
+
+        for (int i = 0; i < nilaiMhs.length; i ++) {
+            System.out.print("Masukkan Nilai Mahasiswa ke -"+(i+1)+" : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                mhsLulus++;
+            }
+        }
+
+        rata2 = total / nilaiMhs.length;
+        System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Mahasiswa yang lulus = " + mhsLulus);
+    }
+}
