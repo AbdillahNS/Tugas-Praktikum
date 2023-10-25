56d66171c70f56ca9e02b2e35866b2716683d2d3
abdillah <abdillahnoersaid@gmail.com>
2023-10-25 10:31:28 +0700

    Modifikasi percobaan 4

--- Jobsheet9/LinearSearch01.java
+++ Jobsheet9/LinearSearch01.java
@@ -1,9 +1,22 @@
+import java.util.*;
 public class LinearSearch01 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jmlElemen = sc.nextInt();
 
-        int[] arrayInt = {34, 28, 26, 48, 72, 20, 56, 63};
+        int[] arrayInt = new int[jmlElemen];
         int key = 20;
         int hasil = 0;
+        
+        for (int i = 0; i < jmlElemen; i++) {
+            System.out.print("Masukkan elemen array ke-" + (i) + ": ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = sc.nextInt();
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
@@ -12,7 +25,7 @@ public class LinearSearch01 {
             }
         }
 
-        System.out.println("Key ada dalam array pada posisi indeks ke-" +hasil);
+        System.out.println("Key ada di posisi indeks ke-" + hasil);
     
     }
 }
