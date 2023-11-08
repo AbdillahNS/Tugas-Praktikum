8cb2ec328a0f35a721167ce96fd02b37fa192280
abdillah <abdillahnoersaid@gmail.com>
2023-11-08 12:24:00 +0700

    Tugas Individu Jobsheet11

---
+++ Jobsheet11/Individu101.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Individu101 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N : ");
+        int N = sc.nextInt();
+
+        if (N < 3) {
+            System.out.println();
+        } else {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N - i; j++) {
+                    System.out.print(" ");
+                }
+
+                for (int k = 1; k <= i; k++) {
+                    System.out.print(k);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
