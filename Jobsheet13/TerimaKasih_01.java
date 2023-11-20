7c50f8cb1556d859f980375757ec6ffb8cdc4d55
abdillah <abdillahnoersaid@gmail.com>
2023-11-20 14:34:04 +0700

    Modifikasi Percobaan 1

--- Jobsheet13/TerimaKasih_01.java
+++ Jobsheet13/TerimaKasih_01.java
@@ -3,4 +3,4 @@ public class TerimaKasih_01 {
     System.out.println("Thank you for being the best teacher in the world.\n" +
         "You inspired in me a love for learning and made me fell like I could ask you anything.");
     }
-}
+}   
