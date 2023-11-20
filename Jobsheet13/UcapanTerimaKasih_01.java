997f0ffa66f895259fe69bee828425917dae5c10
abdillah <abdillahnoersaid@gmail.com>
2023-11-20 14:47:48 +0700

    Modifikasi Percoaan 4

--- Jobsheet13/UcapanTerimaKasih_01.java
+++ Jobsheet13/UcapanTerimaKasih_01.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
+
 public class UcapanTerimaKasih_01 {
-    public static String PenerimaUcapan(){
+    public static String PenerimaUcapan() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Tulisakan NAMA orang yang ingin Anda beri Ucapan: ");
+        System.out.println("Tuliskan NAMA orang yang ingin Anda beri Ucapan: ");
         String namaOrang = sc.nextLine();
         sc.close();
         return namaOrang;
     }
-    public static void UcapanTerimaKasih(){
-        String nama = PenerimaUcapan();
-        System.out.println("Thank you "+nama+" for being the best teacher in the world.\n" +
-        "You inspired in me a love for learning and made me fell like I could ask you anything.");
+
+    public static void UcapanTambahan(String tambahan) {
+        System.out.println("Ucapan Tambahan: " + tambahan);
     }
+
+    public static void UcapanTerimaKasih() {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you " + nama + " for being the best teacher in the world.\n" +
+                "You inspired in me a love for learning and made me feel like I could ask you anything.");
+
+        // Fungsi UcapanTambahan
+        UcapanTambahan("Mantap");
+    }
+
     public static void main(String[] args) {
         UcapanTerimaKasih();
     }
