f8a6e9c87fb1bb832d944f0775d6cd70426da483
abdillah <abdillahnoersaid@gmail.com>
2023-11-20 13:56:54 +0700

    Modifikasi Percobaan 1

--- Jobsheet13/TerimaKasih_01.java
+++ Jobsheet13/TerimaKasih_01.java
@@ -1,14 +1,6 @@
 public class TerimaKasih_01 {
-    public static void UcapanTerimaKasih() {
+    public static void main(String[] args) {
     System.out.println("Thank you for being the best teacher in the world.\n" +
         "You inspired in me a love for learning and made me fell like I could ask you anything.");
-    }
-    public static void UcapanTambahan(String ucapan){
-        System.out.println(ucapan);
-    }
-    public static void main(String[] args) {
-        UcapanTerimaKasih();
-        String ucapan = "Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
-        UcapanTambahan(ucapan);
     }
 }
