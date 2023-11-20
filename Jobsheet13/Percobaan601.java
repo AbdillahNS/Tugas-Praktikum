5e3f4e8818639321a5021bac99baffa282638dbe
abdillah <abdillahnoersaid@gmail.com>
2023-11-20 13:48:06 +0700

    Percobaan 6

--- Jobsheet13/Percobaan601.java
+++ Jobsheet13/Percobaan601.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
 public class Percobaan601 {
-    public static void main(String[] args) {
+    // Fungsi hitungLuas
+    static int hitungLuas (int pjg, int lb){
+        int Luas=pjg*lb;
+        return Luas;
+    }
+    //Fungsi hitungVolume
+    static int hitungVolume (int tinggi, int a, int b){
+        int volume = hitungLuas(a,b)*tinggi;
+        return volume;
+    }
+    //Fungsi main
+    public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
-
         int p, l, t, L, vol;
-
         System.out.println("Masukkan panjang");
         p=input.nextInt();
         System.out.println("Masukkan lebar");
@@ -12,10 +21,9 @@ public class Percobaan601 {
         System.out.println("Masukkan tinggi");
         t=input.nextInt();
 
-        L=p*l;
-        System.out.println("Luasaan panjang adalah "+L);
-
-        vol=p*l*t;
-        System.out.println("Volume balok adalah "+vol);
+        L=hitungLuas(p, l);
+        System.out.println("Luas Persegi Panjang adalah "+L);
+        vol=hitungVolume(t, p, l);
+        System.out.println("Volume Balok adalah "+vol);
     }
 }
