46bda709ad1cfef0068adb4e49a6b26fb0082754
abdillah <abdillahnoersaid@gmail.com>
2023-11-27 15:49:38 +0700

    Modifikasi Percobaan 2 nomer 2

--- Jobsheet14/Percobaan201.java
+++ Jobsheet14/Percobaan201.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
+
 public class Percobaan201 {
-    static int hitungPangkat (int x, int y) {
+    static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return (1);
+            return 1;
         } else {
-            return (x * hitungPangkat(x, y - 1));
+            System.out.print(x);
+            if (y > 1) {
+                System.out.print("x");
+            }
+            return x * hitungPangkat(x, y - 1);
         }
     }
+
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int bilangan, pangkat;
+        Scanner sc = new Scanner(System.in);
+        int bilangan, pangkat;
 
-    System.out.print("Bilangan yang dihitung: ");
-    bilangan = sc.nextInt();
-    System.out.print("Pangkat: ");
-    pangkat = sc.nextInt();
+        System.out.print("Bilangan yang dihitung: ");
+        bilangan = sc.nextInt();
+        System.out.print("Pangkat: ");
+        pangkat = sc.nextInt();
 
-    System.out.println(hitungPangkat(bilangan, pangkat));
+        System.out.print("Hasil dari " + bilangan + "^" + pangkat + " = ");
+        int hasil = hitungPangkat(bilangan, pangkat);
+        System.out.println("x1" +" = " + hasil);
     }
 }
